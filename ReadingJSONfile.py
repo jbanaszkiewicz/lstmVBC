c2b7e3f614d8eec963324587b104d6708643b26d
Kuba <jakub.banaszkiewicz@gmail.com>
2018-08-01 16:25:01 +0200

    Zsynchronizowanie nowego układu współrzędnych z funkcją wczytywania danych

--- ReadingJSONfile.py
+++ ReadingJSONfile.py
@@ -14,6 +14,20 @@ def read_from_file(json_file_name):
         return [input_list]
 
 
+def position_coordinate_system(dict_joints):
+    for joint in dict_joints:
+        if joint != 'SpineMid':
+            dict_joints[joint][0] = dict_joints[joint][0] - dict_joints['SpineMid'][0]
+            dict_joints[joint][1] = dict_joints[joint][1] - dict_joints['SpineMid'][1]
+            dict_joints[joint][2] = dict_joints[joint][2] - dict_joints['SpineMid'][2]
+    for joint in dict_joints:
+        if joint == 'SpineMid':
+            dict_joints['SpineMid'][0] = 0
+            dict_joints['SpineMid'][1] = 0
+            dict_joints['SpineMid'][2] = 0
+    return dict_joints
+
+
 def get_joints(json_file_name, previous_list_input, previous_list_output):
     data = read_from_file(json_file_name)
     # unzipping the data
@@ -29,28 +43,44 @@ def get_joints(json_file_name, previous_list_input, previous_list_output):
 
     for t in data3:
         for dictionaries in t:
+            dict_with_coordinates = {}
             if dictionaries == "Tag":
                 output_data.append(t[dictionaries])
             body = t["Body"]
             if dictionaries == "Body":
                 help_table = []
-                for particular_joint_name in range(len(list_of_joints)):
+                dict_with_coordinates = {}
+                for particular_joint_nr in range(len(list_of_joints)):
                     help_table2 = []
-                    particular_joint = body[list_of_joints[particular_joint_name]]
+                    particular_joint = body[list_of_joints[particular_joint_nr]]
+                    # print(type(list_of_joints[particular_joint_nr]))
                     help_table2.append(particular_joint["X"])
                     help_table2.append(particular_joint["Y"])
                     help_table2.append(particular_joint["Z"])
+                    dict_with_coordinates[list_of_joints[particular_joint_nr]] = help_table2
+                    #print()
+                    # print(type(dict_with_coordinates))
+
+                dict_with_coordinates = position_coordinate_system(dict_with_coordinates)
+
+
+                for particular_joint_nr in range(len(list_of_joints)):
+                    help_table2 = []
+                    particular_joint = dict_with_coordinates[list_of_joints[particular_joint_nr]]
+                    help_table2.append(particular_joint[0])
+                    help_table2.append(particular_joint[1])
+                    help_table2.append(particular_joint[2])
                     help_table.append(help_table2)
-
                 input_data.append(help_table)
-    print()
-
     return input_data, output_data
+
+
 
 
 input_data = []
 output_data = []
 input_data, output_data=get_joints("testexp.json", input_data, output_data)
+print(np.shape(input_data))
 
 # input_data, output_data=get_joints("testexp2.json", input_data, output_data)
 
